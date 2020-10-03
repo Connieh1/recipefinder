124c30ef2d90f5676f0a5e85d350bd7e3cc124c1
github username <constance.hyman@mchsi.com>
2020-10-03 12:14:11 -0700

    Set up setRecipes fn and action, reducer, and store

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -6,23 +6,54 @@ import {
   FormLabel,
   Button,
 } from "react-bootstrap";
+import { connect } from "react-redux";
+import { setRecipes } from "../actions";
 
 class SearchRecipes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ingredients: "",
+      dish: "",
+    };
+  }
+  search() {
+    let { ingredients, dish } = this.state;
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.props.setRecipes(data);
+      });
+  }
+
   render() {
     return (
       <Form inline>
         <FormGroup>
           <FormLabel>Ingredients</FormLabel>{" "}
-          <FormControl type="text" placeholder="garlic, chicken" />
+          <FormControl
+            type="text"
+            placeholder="garlic, chicken"
+            onChange={(event) =>
+              this.setState({ ingredients: event.target.value })
+            }
+          />
         </FormGroup>{" "}
         <FormGroup>
           <FormLabel>Dish</FormLabel>
-          <FormControl type="text" placeholder="adobo"></FormControl>
+          <FormControl
+            type="text"
+            placeholder="adobo"
+            onChange={(event) => this.setState({ dish: event.target.value })}
+          />
         </FormGroup>{" "}
-        <Button>Submit</Button>
+        <Button onClick={() => this.search()}>Submit</Button>
       </Form>
     );
   }
 }
 
-export default SearchRecipes;
+export default connect(null, { setRecipes })(SearchRecipes);
