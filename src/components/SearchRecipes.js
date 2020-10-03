cc96e163559d240d7c2a369acee87e59e845353e
github username <constance.hyman@mchsi.com>
2020-10-03 05:05:16 -0700

    first commit

---
+++ src/components/SearchRecipes.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Button,
+} from "react-bootstrap";
+
+class SearchRecipes extends Component {
+  render() {
+    return;
+    <Form inline>
+      <FormGroup>
+        
+      </FormGroup> <FormGroup></FormGroup>
+    </Form>;
+  }
+}
+
+export default SearchRecipes;
